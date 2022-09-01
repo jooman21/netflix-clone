7288fd883de14785f29fce0f33c1a7c449df5f57
jooman21 <jooman2121@gmail.com>
2022-09-01 11:25:06 +0300

    netflix-clone

---
+++ netflix-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBsj_70YGWn5YEFF3VyCxCikHJQojAosbo",
+    authDomain: "netflix-clone-627df.firebaseapp.com",
+    projectId: "netflix-clone-627df",
+    storageBucket: "netflix-clone-627df.appspot.com",
+    messagingSenderId: "1084788270322",
+    appId: "1:1084788270322:web:4b56e541a94950a8c8dc6b"
+  };
